6f2b9d1efb4d0726f22e4ac89943942307b9ce04
Robert Hernandez Arenas <roberthernandezarenas@gmail.com>
2026-02-16 00:02:25 +0100

    🔧 Fixed NuxtLayout render + fixed aos module

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,6 @@
 <template>
-    <NuxtLayout>caca</NuxtLayout>
+  <div class="bg-red-50 p-4">
+    <h1 class="text-2xl font-bold">Dashboard Home</h1>
+    <p>Welcome to the dashboard.</p>
+  </div>
 </template>
